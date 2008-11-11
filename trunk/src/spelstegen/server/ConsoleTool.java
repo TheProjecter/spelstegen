1fe7cf1c1bfd168ad4fc065088e5dde757693258
hnordin <hnordin@99e55378-a198-11dd-932a-8f5888711c47>
2008-11-11 15:03:20 +0000

    Added ConsoleTool for administration and updated addPlayer SQL in storage implementation.

---
+++ trunk/src/spelstegen/server/ConsoleTool.java
@@ -0,0 +1,77 @@
+package spelstegen.server;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+
+import spelstegen.client.Player;
+
+/**
+ * This is the main class of the console administration tool.
+ * 
+ * @author Henrik Segesten
+ */
+public class ConsoleTool {
+	
+	private StorageInterface storage = new MySQLStorageImpl();
+
+	public static void main(String[] args) {
+		ConsoleTool ct = new ConsoleTool();
+		if (args.length == 0) {
+			System.out.println("Välkommen till administrationsverktyget för spelstegen!");
+			System.out.println("");
+			System.out.println("");
+			System.out.println("Ange ett av följande alternativ och ge som argument till verktyget:");
+			System.out.println("");
+			System.out.println("");
+			System.out.println("1 - Lägg till spelare");
+		}
+		if (args.length == 1) {
+			if (Integer.parseInt(args[0].trim()) == 1) {
+				ct.addPlayer();
+			}
+		}
+	}
+	
+	
+	private void addPlayer() {
+		String name;
+		String email;
+		String nickName;
+		String password;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Skriv in ditt namn");
+		name = sc.nextLine();
+		System.out.println("Skriv in din email");
+		email = sc.nextLine();
+		System.out.println("Skriv in ditt lösenord");
+		String temp = sc.nextLine();
+		System.out.println("Skriv in ditt lösenord igen");
+		password = sc.nextLine();
+		if (password.equals(temp)) {
+			password = getMd5Digest(temp);
+		}
+		System.out.println("Skriv in ditt smeknamn");
+		nickName = sc.nextLine();
+		Player p = new Player(name, email);
+		p.setEncryptedPassword(password);
+		p.setNickName(nickName);
+		storage.addPlayer(p);
+	}
+	
+	static String getMd5Digest(String input) {
+        try
+        {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] messageDigest = md.digest(input.getBytes());
+            BigInteger number = new BigInteger(1,messageDigest);
+            return number.toString(16);
+        }
+        catch(NoSuchAlgorithmException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+	
+}
